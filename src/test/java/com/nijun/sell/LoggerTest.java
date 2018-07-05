d93619b982573b3f2b65111b669f05726de64d37
SmartNi <nijun717@gmail.com>
2018-07-05 20:40:45 +0800

    初始化项目

---
+++ src/test/java/com/nijun/sell/LoggerTest.java
@@ -0,0 +1,32 @@
+package com.nijun.sell;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nijun
+ * Date: 2018/7/5
+ * Time: 7:54 PM
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest {
+
+    @Test
+    public void test1() {
+        String name = "nijun";
+        String password = "123456";
+        log.info("name: {}, password: {}", name, password);
+        log.debug("debug...");
+        log.info("info...");
+        log.error("error...");
+    }
+}
